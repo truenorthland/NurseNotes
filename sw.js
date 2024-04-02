b8638deaad4848f965488a63221b61782a4adcb9
truenorthland <165834860+truenorthland@users.noreply.github.com>
2024-04-02 18:07:19 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,58 +1,83 @@
-// Names of the cache(s)
+// Define a cache name, which helps in versioning and updating the cache later
 var CACHE_NAME = 'nurse-notes-v1';
+
+// Specify all the resources you want to cache
 var urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  // Add other resources and assets to cache
-  // Remember to add icons used in manifest.json
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
-  // Include other paths and extensions as necessary
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    // You can add more resources here
 ];
 
-// Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+// The 'install' event is the first event triggered in the service worker lifecycle
+// Use it to cache all initial resources
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-// Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+// The 'fetch' event is triggered for every network request made by your application
+// Use it to serve cached content when offline and update the cache with new content when online
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response from cache
+                if (response) {
+                    return response;
+                }
+
+                // IMPORTANT: Clone the request. A request is a stream and can only be consumed once.
+                // Since we are consuming this once by cache and once by the browser for fetch, we need to clone the response.
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
 });
 
-// Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['nurse-notes-v1'];
+// The 'activate' event is a good place to clean up old caches
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = [CACHE_NAME]; // Add any other cache names you want to keep
 
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    // Delete the caches that are not in the whitelist
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
